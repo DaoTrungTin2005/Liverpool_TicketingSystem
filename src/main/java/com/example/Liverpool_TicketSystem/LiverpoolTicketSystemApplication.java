08d0a67fd0c9340fd9e02921d00a96aae7886820
Tín <Trungtin0701200@gmail.com>
2025-06-03 07:55:33 +0700

    first commit

---
+++ src/main/java/com/example/Liverpool_TicketSystem/LiverpoolTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Liverpool_TicketSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiverpoolTicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiverpoolTicketSystemApplication.class, args);
+	}
+
+}
