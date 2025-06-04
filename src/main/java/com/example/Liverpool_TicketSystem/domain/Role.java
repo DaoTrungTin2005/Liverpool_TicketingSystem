6204a042679c69ac1c5bc53aca9056c8ddfb3d31
Tín <Trungtin0701200@gmail.com>
2025-06-04 10:22:02 +0700

    Thêm domain : User. Role , và RegisterDTO

--- src/main/java/com/example/Liverpool_TicketSystem/domain/Role.java
+++ src/main/java/com/example/Liverpool_TicketSystem/domain/Role.java
@@ -1,5 +1,68 @@
 package com.example.Liverpool_TicketSystem.domain;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles") // Chỉ định tên bảng là roles
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+
+    // 1 role có nhiều user sử dụng (nhiều user có thể có cùng 1 role)
+    // 1 user chỉ có 1 role
+    @OneToMany (mappedBy = "role")
+
+    // Một đối tượng Role sẽ chứa danh sách tất cả các User có cùng role đó.
+    // Vì 1 role có thể có nhiều user, nên ta dùng @OneToMany để ánh xạ quan hệ này.
+    private List<User> users;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "Role [id=" + id + ", name=" + name + ", description=" + description + ", users=" + users + "]";
+    }
+
+    
     
 }
--- src/main/java/com/example/Liverpool_TicketSystem/domain/User.java
+++ src/main/java/com/example/Liverpool_TicketSystem/domain/User.java
@@ -1,5 +1,87 @@
 package com.example.Liverpool_TicketSystem.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+// Đánh dấu lớp là một thực thể JPA
+@Entity
+
+// chỉ định rõ tên bảng trong database là users
+
+// Nếu không dùng @Table(name = "users") thì mặc định JPA sẽ lấy tên bảng là
+// user (theo tên class, viết thường).
+@Table(name = "users")
+
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String username;
+
+    // 1 role có nhiều user sử dụng (nhiều user có thể có cùng 1 role)
+    // 1 user chỉ có 1 role
+    @ManyToOne
+
+    // Chỉ định cột role_id trong bảng users là khóa ngoại liên kết tới bảng roles.
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", email=" + email + ", password=" + password + ", username=" + username + ", role="
+                + role + "]";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
